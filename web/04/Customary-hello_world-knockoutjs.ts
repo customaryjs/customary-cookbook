2a7ccb7c5f57a30753a8dd1e7a18b0f8a0e8be28
André de Oliveira <andre@arbo.com.br>
2024-09-28 13:26:01 -0600

    better ko

--- web/04/Customary-hello_world-knockoutjs.ts
+++ web/04/Customary-hello_world-knockoutjs.ts
@@ -1,15 +1,20 @@
 import {Customary} from 'customary/Customary.js';
-import * as ko from 'knockout';
-
-const kog = ko;
-const koh = globalThis.ko;
+import * as ko_namespace from 'knockout';
 
 export async function defineCustomElements() {
     await Customary.define();
     const model = {hello: 'Hello', world: 'Customary'};
     const element = document.getElementsByTagName('hello-world')[0];
-    const documentFragment: Node = element.shadowRoot!.getRootNode();
+    const documentFragment = element.shadowRoot!.getRootNode() as DocumentFragment;
+    applyBindingsToDocumentFragment(documentFragment, model);
+}
+
+function applyBindingsToDocumentFragment(documentFragment: DocumentFragment, model: any) {
+    const ko = 'applyBindings' in ko_namespace ? ko_namespace : globalThis.ko;
+
     documentFragment.childNodes.forEach((node: Node) => {
-        if (node.nodeType === 1 || node.nodeType === 8) koh.applyBindings(model, node);
+        if (node.nodeType === 1 || node.nodeType === 8) {
+            ko.applyBindings(model, node);
+        }
     });
 }

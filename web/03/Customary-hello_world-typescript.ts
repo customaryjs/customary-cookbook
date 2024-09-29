03db5c1bfc2597d7954f175388f04abace3f5b6e
André de Oliveira <andre@arbo.com.br>
2024-09-29 00:11:33 -0600

    Customary State

--- web/03/Customary-hello_world-typescript.ts
+++ web/03/Customary-hello_world-typescript.ts
@@ -1,5 +1,13 @@
 import {Customary} from 'customary/Customary.js';
 
-export async function defineCustomElements() {
+export async function populate() {
     await Customary.define();
+
+    document.getElementById('populate-here')!.append(
+        document.createElement('hello-world-part-1'),
+        document.createTextNode(' '),
+        document.createElement('hello-world-part-2'),
+        document.createTextNode(' '),
+        document.createElement('hello-world-part-3'),
+    );
 }
--- web/04/Customary-hello_world-knockoutjs.ts
+++ web/04/Customary-hello_world-knockoutjs.ts
@@ -1,20 +1,13 @@
 import {Customary} from 'customary/Customary.js';
-import * as ko_namespace from 'knockout';
+import {KnockoutBridge} from "customary/knockoutjs/KnockoutBridge.js";
 
-export async function defineCustomElements() {
+export async function bind() {
     await Customary.define();
+
     const model = {hello: 'Hello', world: 'Customary'};
-    const element = document.getElementsByTagName('hello-world')[0];
-    const documentFragment = element.shadowRoot!.getRootNode() as DocumentFragment;
-    applyBindingsToDocumentFragment(documentFragment, model);
-}
 
-function applyBindingsToDocumentFragment(documentFragment: DocumentFragment, model: any) {
-    const ko = 'applyBindings' in ko_namespace ? ko_namespace : globalThis.ko;
+    const element = document.querySelector('hello-world');
+    const documentFragment = element!.shadowRoot!.getRootNode() as DocumentFragment;
 
-    documentFragment.childNodes.forEach((node: Node) => {
-        if (node.nodeType === 1 || node.nodeType === 8) {
-            ko.applyBindings(model, node);
-        }
-    });
+    new KnockoutBridge().applyBindingsToDocumentFragment(model, documentFragment);
 }

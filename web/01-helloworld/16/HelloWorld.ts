20ca74daf91c857d6c7f532c7ebcf9674ae796b6
André de Oliveira <andre@arbo.com.br>
2024-10-12 22:11:26 -0600

    Customary Hooks

--- web/01-helloworld/16/HelloWorld.ts
+++ web/01-helloworld/16/HelloWorld.ts
@@ -2,5 +2,5 @@ import {CustomaryHTMLElement} from "customary/CustomaryHTMLElement.js";
 import {CustomaryOptions} from "customary/CustomaryOptions.js";
 
 export class HelloWorld extends CustomaryHTMLElement {
-    static customary: CustomaryOptions<any> = {name: 'hello-world', externalLoaderOptions: {import_meta: import.meta}}
+    static customary: CustomaryOptions = {name: 'hello-world', externalLoaderOptions: {import_meta: import.meta}}
 }
--- web/01-helloworld/16/HelloWorldPart1.ts
+++ web/01-helloworld/16/HelloWorldPart1.ts
@@ -2,5 +2,5 @@ import {CustomaryHTMLElement} from "customary/CustomaryHTMLElement.js";
 import {CustomaryOptions} from "customary/CustomaryOptions.js";
 
 export class HelloWorldPart1 extends CustomaryHTMLElement {
-    static customary: CustomaryOptions<any> = {name: 'hello-world-part-1', externalLoaderOptions: {import_meta: import.meta}}
+    static customary: CustomaryOptions = {name: 'hello-world-part-1', externalLoaderOptions: {import_meta: import.meta}}
 }

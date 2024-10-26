7dd51f1e5141107d1c36e501ea487ace83442d0e
André de Oliveira <andre@arbo.com.br>
2024-10-25 18:13:53 -0600

    with a list

---
+++ web/02-state/06/test-Customary-state-list.ts
@@ -0,0 +1,26 @@
+import 'mocha';
+import {CustomaryTestingQueries} from "customary/testing/CustomaryTestingQueries.js";
+import {page_html_of_test_js, suite} from "../../test/suite.js";
+
+const PAGE = page_html_of_test_js(import.meta);
+
+describe(suite(PAGE), async function (){
+    this.timeout(4000);
+    this.slow(500);
+
+    let window: Window;
+
+    before(() => window = globalThis.window.open(PAGE)!);
+    after(() => window.close());
+
+    describe('on page load', async function () {
+        it('render expected custom element text content', async function () {
+            this.retries(32);
+            CustomaryTestingQueries.findByTextContent(
+                window.document.querySelector('tutorial-card')!.shadowRoot!,
+                "02-state/06/test-Customary-state-list.ts",
+                {selector: 'div.c > div'}
+            );
+        });
+    });
+});

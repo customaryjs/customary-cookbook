91286ad5739eb22ef8d94b7b02122bd753cca553
André de Oliveira <andre@arbo.com.br>
2024-11-25 21:46:45 -0700

    attributes with lit

--- web/01-helloworld/03/test-Customary-helloworld-attribute.ts
+++ web/01-helloworld/03/test-Customary-helloworld-attribute.ts
@@ -15,7 +15,7 @@ describe(suite.title, async function (){
 
     describe('on page load', async function () {
         it('render expected custom element text content', async function () {
-            this.retries(32);
+            this.retries(64);
             CustomaryTestingQueries.findByTextContent(window.document, 'Hello Customary !', {selector: 'h1'});
         });
     });

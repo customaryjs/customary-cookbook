b36c6783faf0df7ee71a302817376cbdeaf21d05
André de Oliveira <andre@arbo.com.br>
2024-12-26 22:08:07 -0700

    changes hooks

--- web/06-attributes/04/AttributesComponent.ts
+++ web/06-attributes/04/AttributesComponent.ts
@@ -16,17 +16,13 @@ export class AttributesComponent extends CustomaryLitElement {
 	static customary: CustomaryOptions<AttributesComponent> = {
 		config: {name: 'attributes-component'},
 		state: {'notes': ["C", "D", "E", "F", "G", "A", "B"]},
-		hooks: {externalLoader: {import_meta: import.meta}}
-	}
-
-	// noinspection JSUnusedGlobalSymbols
-	override updated(changedProperties: Map<string, any>) {
-		super.updated(changedProperties);
-		if (changedProperties.has('chord')) {
-			const el = this;
-			const value = el['chord'];
-			for (const id of value.split('')) {
-				el.shadowRoot!.getElementById(id)!.classList.add('chord');
+		hooks: {
+			externalLoader: {import_meta: import.meta},
+			changes: {
+				'chord': (el: AttributesComponent, a: string) =>
+						a.split('').forEach(
+							id => el.shadowRoot!.getElementById(id)!.classList.add('chord')
+						)
 			}
 		}
 	}

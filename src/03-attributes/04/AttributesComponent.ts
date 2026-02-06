934b018b99583443041c0c98c0a9aef8a14bd882
André de Oliveira <andre@arbo.com.br>
2026-02-05 20:53:30 -0700

    Customary Markup: classMap

--- src/03-attributes/04/AttributesComponent.ts
+++ src/03-attributes/04/AttributesComponent.ts
@@ -1,31 +1,25 @@
 import {CustomaryElement, CustomaryDeclaration, Customary} from "#customary";
-import {property} from "@lit/reactive-element/decorators/property.js";
 
 // noinspection JSUnusedGlobalSymbols
 export class AttributesComponent extends CustomaryElement {
 
-	/*
-		!!! IMPORTANT !!!
-		@property requires in tsconfig.json:
-		"useDefineForClassFields": false
-		https://lit.dev/docs/components/decorators/#decorators-typescript
-	 */
-	@property()
-	chord: string = '';
-
 	static customary: CustomaryDeclaration<AttributesComponent> = {
 		name: 'attributes-component',
 		values: {'notes': ["C", "D", "E", "F", "G", "A", "B"]},
+		config: {
+			attributes: ['chord'],
+			state: ['scale'],
+		},
 		hooks: {
 			externalLoader: {import_meta: import.meta},
 			changes: {
 				'chord': (el: AttributesComponent, a: string) =>
-						a.split('').forEach(
-							id => el.shadowRoot!.getElementById(id)!.classList.add('chord')
-						)
-			}
-		}
+					el.scale = el.notes.map(note => ({note, bright: a.includes(note)})),
+			},
+		},
 	}
+	declare notes: string[];
+	declare scale: {note: string, bright: boolean}[];
 }
 
 Customary.declare(AttributesComponent);
